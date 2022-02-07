47fa271fd582a6ed53f82a872ad42d941076885e
Vinicius Axt <viniciusaxt@gmail.com>
2022-02-07 18:52:13 -0300

    Added express

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const { default: axios } = require('axios');
 const cron = require('node-cron');
 
-cron.schedule('* * * * * *', () => {
-  console.log('⏰ Task a cada 1 segundo')
-  axios.post('http://localhost:1337/cronjob/every-second')
-    .then(({data}) => console.log({ data }))
-    .catch(err => console.log({ err: err.message }))
-});
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send({ success: true })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+
+  cron.schedule('* * * * * *', () => {
+    console.log('⏰ Task a cada 1 segundo')
+    axios.post('http://localhost:1337/cronjob/every-second')
+      .then(({data}) => console.log({ data }))
+      .catch(err => console.log({ err: err.message }))
+  });
+})
