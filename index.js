cec3096761c2206a63f7308c4f872d27a7e9e3b0
Vinicius Axt <viniciusaxt@gmail.com>
2022-02-16 14:58:11 -0300

    Salve

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 
-  console.log(`Pod mega novo!!!`)
+  console.log(`Salve rapeiz`)
 
 
   cron.schedule('* * * * * *', () => {
