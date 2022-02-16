c0866309e64efe16227308b2be3d2af36081a6cc
Vinicius Axt <viniciusaxt@gmail.com>
2022-02-16 11:57:15 -0300

    Fixed fleet deployment error

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 
-  console.log(`Pod super novo!!!`)
+  console.log(`Pod mega novo!!!`)
 
 
   cron.schedule('* * * * * *', () => {
